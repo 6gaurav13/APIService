7d9b8586f32c7623c4d6d242c55558196850556f
6gaurav13 <6gaurav13@gmail.com>
2025-04-07 01:31:35 +0530

    Revert "Merge pull request #3 from 6gaurav13/threadpolling"
    This reverts commit 950b7135a3958f002bb14906f7fb20a80a2f79ea, reversing
    changes made to 4a45a168c55c51e46bf3ab875f017965b63ea827.

--- IntegrationService/src/main/java/com/example/IntegrationService/task/OperationTask.java
+++
@@ -1,8 +0,0 @@
-package com.example.IntegrationService.task;
-
-import com.example.IntegrationService.model.IntegrationConfig;
-
-public interface OperationTask extends Runnable {
-	
-	void setConfig(IntegrationConfig config);
-}
